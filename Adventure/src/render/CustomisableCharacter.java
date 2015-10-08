bc42bf3cc5e2c6f378a6f387a9bf8805ffff7c62
Paige-H <paige@paratu.net>
2015-10-08 13:02:35 +1300

    Updated CustomisableCharacter draw method

--- Adventure/src/render/CustomisableCharacter.java
+++ Adventure/src/render/CustomisableCharacter.java
@@ -11,22 +11,77 @@ import render.RenderCanvas.Compass;
  *
  */
 public class CustomisableCharacter extends Drawable {
-	
-	private AnimationSequence hatImages; 
+
+	private AnimationSequence hatImages;
+	private Direction dir = Direction.UP;
 
 	public CustomisableCharacter(String characterImgLoc, String hatString) {
 		super(characterImgLoc, Position.CENTER);
 		hatImages = new AnimationSequence(hatString);
 	}
 
+	public enum Direction {
+		UP, LEFT, RIGHT, DOWN
+	}
+
 	@Override
-	public void draw(Compass direction, int x, int y, Graphics g, int tileWidth,
-			int tileHeight) {
-		super.draw(direction, x, y, g, tileWidth, tileHeight);
-		BufferedImage hatImage = hatImages.getImage(direction);
-		int xPos =x + (tileWidth / 2) - (hatImage.getWidth() / 2);
-		int yPos =super.getYOnScreen()  - hatImage.getHeight();
+	public void draw(Compass direction, int x, int y, Graphics g,
+			int tileWidth, int tileHeight) {
+		Compass realDirection = getRelativeDirectionFromCompass(direction);
+		super.draw(realDirection, x, y, g, tileWidth, tileHeight);
+		BufferedImage hatImage = hatImages.getImage(realDirection);
+		int xPos = x + (tileWidth / 2) - (hatImage.getWidth() / 2);
+		int yPos = super.getYOnScreen() - hatImage.getHeight();
 		g.drawImage(hatImage, xPos, yPos, null);
+	}
+
+	private Compass getRelativeDirectionFromCompass(Compass direction) {
+		int intValue;
+		switch (dir) {
+		case UP:
+			return direction;
+		case LEFT:
+			intValue = direction.ordinal() -1;
+			if(intValue <0){
+				intValue = 3;
+			}
+			return Compass.values()[intValue];
+		case RIGHT:
+			intValue = direction.ordinal() +1;
+			if(intValue >3){
+				intValue = 0;
+			}
+			return Compass.values()[intValue];
+		case DOWN:
+			intValue = direction.ordinal() +2;
+			if(intValue ==4){
+				intValue = 0;
+			}
+			if(intValue ==5){
+				intValue = 1;
+			}
+			return Compass.values()[intValue];
+		default:
+			return direction;
+		}
+		
+			
+	}
+
+	/**
+	 * @return the direction the character if facing (Different to cardinal
+	 *         direction)
+	 */
+	public Direction getDirection() {
+		return dir;
+	}
+
+	/**
+	 * @param dir
+	 *            the direction to set
+	 */
+	public void setDirection(Direction dir) {
+		this.dir = dir;
 	}
 
 }
